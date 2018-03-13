7d223f9cf99cc130fbb59ba93d7dc0e86effe3a0
Aakash Ravichandran <33166705+pavlovedoncaffeine@users.noreply.github.com>
2018-03-13 18:01:48 -0400

    changed 127.0.0.1 to localhost

--- test-python udp client-server/udp_client_json.py
+++ test-python udp client-server/udp_client_json.py
@@ -4,7 +4,7 @@ import socket
 from datetime import datetime
 import time
 
-UDP_IP = "127.0.0.1"
+UDP_IP = "localhost"
 UDP_PORT = 12345
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
--- test-python udp client-server/udp_server_json.py
+++ test-python udp client-server/udp_server_json.py
@@ -4,7 +4,7 @@ import socket
 from datetime import datetime
 import time
 
-UDP_IP = "127.0.0.1"
+UDP_IP = "localhost"
 UDP_PORT = 12345
 
 
