fa24ec3920e7c38c6ca9ab27a3756cd9e5d67a24
Avyay Agarwal <30388692+AvyayAgarwal@users.noreply.github.com>
2018-05-12 14:43:54 -0400

    serial port added

--- server/usb.js
+++ server/usb.js
@@ -5,3 +5,37 @@ var serialPort = new SerialPort("port name", {
   baudrate: 9600,
   parser: serialport.parsers.readline("\n")
 });
+
+
+
+// serialPort.on("open", function () {
+//   console.log('open');
+//   serialPort.on('data', function(data) {
+//     console.log(data);
+//   });
+// });
+
+
+
+function write() //for writing
+{
+    serialPort.on('data', function (data) 
+    {
+        serialPort.write("Write your data here");
+    });
+}
+
+function read () // for reading
+{
+    serialPort.on('data', function(data)
+    {
+        console.log(data); 
+    });  
+}
+
+serialPort.on('open', function() 
+{
+    // execute your functions
+    write(); 
+    read(); 
+});
