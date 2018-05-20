0cf8964f9bff2cda9b5f1b2ca4d115ab4ddb77aa
Mohammed Nafees <hello@mnafees.me>
2018-05-20 13:36:52 -0400

    Fix crash on Windows for Intel UHD Grpahics 620

--- main.cpp
+++ main.cpp
@@ -4,6 +4,13 @@
 
 int main(int argc, char *argv[])
 {
+    /**
+     * https://bugreports.qt.io/browse/QTBUG-64697?focusedCommentId=380295&page=com.atlassian.jira.plugin.system.issuetabpanels%3Acomment-tabpanel#comment-380295
+     *
+     * All QML apps crash on Intel UHD Grpahics 620
+     * Confirmed on Windows 10, with Qt 5.10.1 MSVC 2017 */
+    QCoreApplication::setAttribute(Qt::AA_DisableShaderDiskCache);
+
 #if defined(Q_OS_WIN)
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
