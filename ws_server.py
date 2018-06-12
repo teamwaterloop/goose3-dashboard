789202bca3588d3812dd28c878f0c44ce9a93020
Aakash Ravichandran <akash.ravichandran@gmail.com>
2018-06-12 04:28:32 -0400

    Update to python server
    Python server now runs multiple processes. One to send data to client constantly and one to check commands send back from server
    
    Required: JSON dummy value for server to ignore when sending commands back from Client.

--- ws_server.py
+++ ws_server.py
@@ -1,44 +1,67 @@
 import logging
 import json
 from random import randint
+import multiprocessing
 import time
 from websocket_server import WebsocketServer as ws
 
-state = ["IDLE", "READY", "ACCEL", "COAST", "BRAKE"]
+#state = ["IDLE", "READY", "ACCEL", "COAST", "BRAKE"]
+
+#msg = {
+#    'acceleration' : 0,
+#    'distance' : 0,
+#    'velocity' : 0,
+#    'propulsion_temp' : 45,
+#    'braking_temp' : 119,
+#    'motherboard_temp' : 38,
+#    'pod_state': state[0]
+#    }
 
 msg = {
-    'acceleration' : 0,
-    'distance' : 0,
-    'velocity' : 0,
-    'propulsion_temp' : 45,
-    'braking_temp' : 119,
-    'motherboard_temp' : 38,
-    'pod_state': state[0]
+    'levitation': 0,
+    'mag_l': 0,
+    'mag_r': 0,
+    'fr_drive' : 0
     }
 
 def send_data(client, server):
-    print("New Client connected.\n")
     print("Sending data:")
-    while server.clients:
-        time.sleep(0.25)
-        msg['acceleration']+=1
-        msg['velocity']+=0.25*msg['acceleration']
-        msg['distance']+=0.25*msg['velocity']
-        msg['pod_state']=state[randint(0,4)]
-        send_msg = json.dumps(msg,  separators=(',',':'))
-        server.send_message(client, send_msg)
-        print(send_msg)
+    send_msg = json.dumps(msg, separators=(',',':'))
+    server.send_message(client, send_msg)
+    print(send_msg)
+
+def new_client(client, server):
+    print("New Client connected.\n")
+    while True:
+        process = multiprocessing.Process(target=send_data, args=(client, server))
+        process.daemon = True
+        process.start()
 
 
 def new_cmd(client, server, message):
     msg_json = json.loads(message)
-    print(msg_json['levitation'] + "\n")
-    print(msg_json['mag_speed'] + "\n")
-    print(msg_json['fr_speed'] + "\n")    
+    msg['levitation'] = msg_json['levitation']
+    #msg['mag_r'] = msg_json['mag_speed']
+    #msg['mag_l'] = msg_json['mag_speed']
+    #msg['fr_drive'] = msg_json['fr_speed']
+        
+    print(msg_json['levitation'])
+    #print(msg_json['mag_speed'] + "\n")
+    #print(msg_json['fr_speed'] + "\n")    
 
     
-json_message = json.dumps(msg)
 server = ws(6500, host='127.0.0.1')
-server.set_fn_new_client(send_data)
-server.set_fn_message_received(new_cmd)
-server.run_forever()
+
+if __name__ == "__main__":
+    server = ws(6500, host='127.0.0.1')
+    try:
+        print ('Server start')
+        server.set_fn_new_client(send_data)
+        server.set_fn_message_received(new_cmd)
+    except:
+        print ('Exception')
+    finally:
+        for process in multiprocessing.active_children():
+            process.terminate()
+            process.join()
+    print ('Goodbye')
